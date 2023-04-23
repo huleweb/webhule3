b5cab03b0305982a7d1b0b077bc8badea49ff6c7
hule <huleweb@gmail.com>
2023-04-23 17:41:11 -0300

    color bermuda

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			
+			'bermuda': ' #FFF200',
+		  },
 		extend: {
 			fontFamily: {
 				worksans: ['Work Sans', 'sans-serif'],
