030c30eb43dceb06abd729bcdcf58f8791ea0ee5
hule <huleweb@gmail.com>
2023-04-23 17:43:18 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			
-			'bermuda': ' #FFF200',
-		  },
+		
 		extend: {
 			fontFamily: {
 				worksans: ['Work Sans', 'sans-serif'],
 				
 				gillsans: ['Gill Sans', 'sans-serif']
                                                 
+			  },
+			  colors: {
+			
+				'bermuda': ' #FFF200',
 			  },
 			  
 		},
